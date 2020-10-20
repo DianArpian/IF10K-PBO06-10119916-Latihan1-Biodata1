a37fad1f886351e01e631c4d7b193e0ffcc2d439
DianArpian <>
2020-10-20 21:17:26 +0700

    Biodata

---
+++ src/pbo06/pkg10119916/latihan1/biodata/PBO0610119916Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo06.pkg10119916.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : DIAN ARPIAN
+ * KELAS    : IF10K
+ * NIM      : 10119916
+ * Deskripsi Program : Perintah Menampilkan Value ke Layar
+ */
+public class PBO0610119916Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("BIODATA ROCSTAR" + "\n" + "-----------------------");
+        System.out.println("NAMA\t\t: DIAN ARPIAN");
+        System.out.println("ALAMAT\t\t: KP JELEKONG NO 1");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 17 APRIL 2000");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: SEPAK BOLA");
+        System.out.println("MAKES\t\t: CAH KANGKUNG");
+        System.out.println("MIKES\t\t: DAWET");
+       
+    }
+    
+}
